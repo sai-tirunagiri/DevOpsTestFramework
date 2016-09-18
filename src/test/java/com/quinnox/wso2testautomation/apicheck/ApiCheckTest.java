52a76cd0a6c7956e5afaed2f2462b4f4f7f255b7
Administrator <sai.tirunagiri@gmail.com>
2016-09-18 23:30:08 +0530

    Adding changes to project

---
+++ src/test/java/com/quinnox/wso2testautomation/apicheck/ApiCheckTest.java
@@ -0,0 +1,45 @@
+package com.quinnox.wso2testautomation.apicheck;
+
+import org.apache.http.HttpResponse;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quinnox.wso2testautomation.exception.ServiceException;
+import com.quinnox.wso2testautomation.model.Employee;
+import com.quinnox.wso2testautomation.service.EmployeeService;
+import com.quinnox.wso2testautomation.service.EmployeeServiceImpl;
+import com.quinnox.wso2testautomation.util.APIUtil;
+import com.quinnox.wso2testautomation.util.APIUtilDTO;
+
+public class ApiCheckTest {
+	APIUtilDTO apiUtilDto;
+	APIUtil apiUtil;
+	EmployeeService employeeService;
+	Employee employee; 
+	
+	
+	@BeforeTest
+	public void init() throws Exception {
+		apiUtilDto = new APIUtilDTO();
+		apiUtil = new APIUtil();
+		employeeService = new EmployeeServiceImpl();
+	}
+	
+	
+	@Test
+	public void verifyGetWso2APIDSSServiceResponse() {
+		System.out.println("TEST 1");
+		apiUtilDto = apiUtil.extractAPITestDataForDSSSevice();
+		int response = 0;
+		try {
+			response = employeeService.getWso2APIDSSServiceResponse(apiUtilDto);
+			Assert.assertEquals(response, 202,
+					"API IS NOT REACHABLE");
+		} catch (ServiceException e) {
+			System.out
+					.println("Went wrong while mocking the call for WSO2 API "
+							+ e.getMessage());
+		}
+	}
+}
---
+++ src/test/java/com/quinnox/wso2testautomation/verifyrecord/VerifyEmployeeRecordTest.java
@@ -0,0 +1,47 @@
+package com.quinnox.wso2testautomation.verifyrecord;
+
+import org.apache.http.HttpResponse;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quinnox.wso2testautomation.exception.ServiceException;
+import com.quinnox.wso2testautomation.model.Employee;
+import com.quinnox.wso2testautomation.service.EmployeeService;
+import com.quinnox.wso2testautomation.service.EmployeeServiceImpl;
+import com.quinnox.wso2testautomation.util.APIUtil;
+import com.quinnox.wso2testautomation.util.APIUtilDTO;
+
+public class VerifyEmployeeRecordTest {
+	APIUtilDTO apiUtilDto;
+	APIUtil apiUtil;
+	EmployeeService employeeService;
+	Employee employee; 
+	
+	
+	@BeforeTest
+	public void init() throws Exception {
+		apiUtilDto = new APIUtilDTO();
+		apiUtil = new APIUtil();
+		employeeService = new EmployeeServiceImpl();
+	}
+	
+	
+		@Test
+		public void verifyGetEmployeeFromDSSService() {
+			System.out.println("TEST 3");
+			apiUtilDto = apiUtil.extractAPITestDataFetchEmployeeForDSSSevice();
+			APIUtilDTO apiUtilDtoForJsonInput = apiUtil.extractAPITestDataForEmployeeJsonInput();
+			boolean response = false;
+			try {
+				employee = employeeService.getEmployeeDataFromInputJson(apiUtilDtoForJsonInput);
+				response = employeeService.getEmployeeFromDSSService(apiUtilDto,employee);
+				Assert.assertEquals(response, true,
+						"Employee Data Not Found in DB");
+			} catch (ServiceException e) {
+				System.out
+						.println("Employee Data Not Found in DB "
+								+ e.getMessage());
+			}
+		}
+}

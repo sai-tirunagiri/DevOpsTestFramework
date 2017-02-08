bc5ce2c73aefe4a10149d047469ecdae0081a8a1
Administrator <sai.tirunagiri@gmail.com>
2017-02-08 20:19:19 +0530

    Adding exact path

--- src/main/java/com/quinnox/wso2testautomation/constants/AppConstants.java
+++ src/main/java/com/quinnox/wso2testautomation/constants/AppConstants.java
@@ -12,8 +12,8 @@ public class AppConstants {
 public static final String EXCEL_SHEET_NAME = "Sheet1";
 	
 	
-	public static final String apiFilePath = "C:\\Users\\narayan\\Documents\\Devops_Server\\API_DB_Dev_API_20160616.xlsx";
+	//public static final String apiFilePath = "C:\\Users\\narayan\\Documents\\Devops_Server\\API_DB_Dev_API_20160616.xlsx";
 	
 	
-	//public static final String apiFilePath = "/jenkins/testdata/API_DB_Dev_API_20160616.xlsx";
+	public static final String apiFilePath = "/jenkins/testdata/API_DB_Dev_API_20160616.xlsx";
 }

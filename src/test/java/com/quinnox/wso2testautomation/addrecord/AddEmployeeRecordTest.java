80e818f438205a61c3b31fcd06f42dac6fe166ab
Administrator <sai.tirunagiri@gmail.com>
2017-02-08 17:35:35 +0530

    Adding tests

---
+++ src/test/java/com/quinnox/wso2testautomation/addrecord/AddEmployeeRecordTest.java
@@ -0,0 +1,39 @@
+package com.quinnox.wso2testautomation.addrecord;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quinnox.wso2testautomation.exception.ServiceException;
+import com.quinnox.wso2testautomation.model.Employee;
+import com.quinnox.wso2testautomation.service.EmployeeService;
+import com.quinnox.wso2testautomation.service.EmployeeServiceImpl;
+import com.quinnox.wso2testautomation.util.APIUtil;
+import com.quinnox.wso2testautomation.util.APIUtilDTO;
+
+public class AddEmployeeRecordTest {
+	APIUtilDTO apiUtilDto;
+	APIUtil apiUtil;
+	EmployeeService employeeService;
+	Employee employee; 
+	
+	
+	@BeforeTest
+	public void init() throws Exception {
+		apiUtilDto = new APIUtilDTO();
+		apiUtil = new APIUtil();
+		employeeService = new EmployeeServiceImpl();
+	}
+	
+	
+	@Test
+	public void verifyAddEployeeFromDSSService() throws ServiceException {
+		apiUtilDto = apiUtil.extractAPITestDataAddEmployeeForDSSSevice();
+		HttpResponse response = employeeService.addEployeeFromDSSService(apiUtilDto);
+		StatusLine statusLine = response.getStatusLine();
+		int statusCode = statusLine.getStatusCode();
+		Assert.assertEquals(statusCode, 200,"RECORDS NOT INSERTED!!!");
+	}	
+}
---
+++ src/test/java/com/quinnox/wso2testautomation/apicheck/ApiCheckTest.java
@@ -0,0 +1,36 @@
+package com.quinnox.wso2testautomation.apicheck;
+
+import org.apache.http.HttpResponse;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quinnox.wso2testautomation.exception.ServiceException;
+import com.quinnox.wso2testautomation.model.Employee;
+import com.quinnox.wso2testautomation.service.EmployeeService;
+import com.quinnox.wso2testautomation.service.EmployeeServiceImpl;
+import com.quinnox.wso2testautomation.util.APIUtil;
+import com.quinnox.wso2testautomation.util.APIUtilDTO;
+
+public class ApiCheckTest {
+	APIUtilDTO apiUtilDto;
+	APIUtil apiUtil;
+	EmployeeService employeeService;
+	Employee employee; 
+	
+	
+	@BeforeTest
+	public void init() throws Exception {
+		apiUtilDto = new APIUtilDTO();
+		apiUtil = new APIUtil();
+		employeeService = new EmployeeServiceImpl();
+	}
+	
+	
+	@Test
+	public void verifyGetWso2APIDSSServiceResponse() throws ServiceException {
+		apiUtilDto = apiUtil.extractAPITestDataForDSSSevice();
+		int response = employeeService.getWso2APIDSSServiceResponse(apiUtilDto);
+		Assert.assertEquals(response, 202,"API IS NOT ACCESSIBLE!!!");
+	}
+}
